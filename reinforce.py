53db7d36c97daf21291509e11f8b0f3ceb206243
Dmitry Vyal <dmitryvyal@gmail.com>
2018-12-08 21:21:49 +0300

    reinforce with a table baseline, gradient clipping, decreasing temperature of a softmax. Able to learn race5

--- reinforce.py
+++ reinforce.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.utils as utils
 from torch.distributions import Categorical
 
 
@@ -19,15 +20,48 @@ class Policy(nn.Module):
         self.inp_diap = torch.FloatTensor(szs)
 
 
-    def forward(self, x):
+    def forward(self, x, t):
         x = (x - self.inp_diap / 2) / self.inp_diap
         x = F.relu(self.affine1(x))
         action_scores = self.affine2(x)
-        return F.softmax(action_scores, dim=1)
+        return F.softmax(action_scores / t, dim=1)
 
-    def select_action(self, state):
+    def select_action(self, state, t=1):
         state = torch.FloatTensor(state).unsqueeze(0)
-        probs = self.forward(state)
+        probs = self.forward(state, t=t)
+        m = Categorical(probs)
+        action = m.sample()
+        return action.item(), m.log_prob(action), torch.sum(-probs * probs.log())
+
+
+class SpatialPolicy(nn.Module):
+    """ Policy with separate input neuron for each spatial state and axe velocity """
+    def __init__(self, env, hidden=128):
+        super().__init__()
+        szs = env.get_sizes()
+        self.szs = szs
+        self.inp_size = szs[0]*szs[1] + szs[2]+szs[3]
+        n_actions = env.N_ACTIONS
+
+        self.affine1 = nn.Linear(szs[0]*szs[1] + szs[2]+szs[3], hidden)
+        self.affine2 = nn.Linear(hidden, n_actions)
+
+    def forward(self, x, t):
+        x = F.relu(self.affine1(x))
+        action_scores = self.affine2(x)
+        return F.softmax(action_scores / t, dim=1)
+
+    def select_action(self, state, t=1):
+        x = torch.zeros([self.inp_size]).float()
+        p0 = 0
+        x[state[0] * self.szs[1] + state[1]] = 1
+        p0 += self.szs[0]*self.szs[1]
+        x[p0 + state[2]] = 1
+        p0 += self.szs[2]
+        x[p0 + state[3]] = 1
+        assert len(state) == 4
+
+        probs = self.forward(x.unsqueeze(0), t=t)
         m = Categorical(probs)
         action = m.sample()
         return action.item(), m.log_prob(action), torch.sum(-probs * probs.log())
@@ -66,6 +100,8 @@ class Reinforce:
         self.running_reward = None
         self.iter = 0
 
+        self.table_value = torch.zeros(env.get_sizes()[:4]).float()
+
         self.actions = []
         self.log_probs = []
         self.entropies = []
@@ -76,7 +112,7 @@ class Reinforce:
         self.env.reset()
         for t in range(self.max_len):  # Don't infinite loop while learning
             st = self.env.get_state()
-            action, log_prob, entropy = self.policy.select_action(st)
+            action, log_prob, entropy = self.policy.select_action(st, t=max(500 / (self.iter+1), 1))
             res, state, reward = self.env.act(action)
             self.actions.append(action)
             self.rewards.append(reward)
@@ -107,13 +143,15 @@ class Reinforce:
         p_deltas = []
         rgains = []
 
-        rvalues = self.value.forward(torch.FloatTensor(self.states[::-1]))
+        #rvalues = self.value.forward(torch.FloatTensor(self.states[::-1]))
 
-        for r, v in zip(self.rewards[::-1], rvalues):
+        for r, s in zip(self.rewards[::-1], self.states[::-1]):
             G = r + self.gamma * G
-            p_deltas.append(G-v)
+            delta = G - self.table_value[s[0], s[1], s[2], s[3]]
+            p_deltas.append(delta)
 #            print(G,v)
             rgains.append(G)
+            self.table_value[s[0], s[1], s[2], s[3]] += 0.1 * delta
         p_deltas = torch.tensor(p_deltas[::-1])
         #gains = (gains - gains.mean()) / (gains.std() + 1e-6)
 
@@ -123,12 +161,15 @@ class Reinforce:
         self.opt.zero_grad()
         self.value_opt.zero_grad()
 
-        policy_loss = torch.cat(policy_loss).mean()
+        policy_loss = torch.cat(policy_loss).mean() - 1 * torch.tensor(self.entropies).mean()
         policy_loss.backward()
+        utils.clip_grad_norm(self.policy.parameters(), 5)
 
-        value_loss = (torch.tensor(rgains) - rvalues)**2
-        value_loss = value_loss.mean()
-        value_loss.backward()
+
+
+        #value_loss = (torch.tensor(rgains) - rvalues)**2
+        #value_loss = value_loss.mean() - torch.tensor(self.entropies).mean()
+        #value_loss.backward()
 
         self.opt.step()
         self.value_opt.step()
@@ -138,7 +179,8 @@ class Reinforce:
             self.writer.add_scalar('gain0', G, self.iter)
             self.writer.add_scalar('episode_len', len(self.rewards), self.iter)
 
-            self.writer.add_scalar('value_loss', value_loss, self.iter)
+            #self.writer.add_scalar('value_loss', value_loss, self.iter)
+            self.writer.add_scalar('mean_delta', torch.tensor(-p_deltas).abs().mean() , self.iter)
             self.writer.add_scalar('entropy', torch.tensor(self.entropies).mean(), self.iter)
 
             for n, p in self.policy.named_parameters():
@@ -151,15 +193,14 @@ class Reinforce:
         self.log_probs.clear()
         self.entropies.clear()
         self.states.clear()
-        self.iter += 1
 
 
 if __name__ == '__main__':
     import race as R
-    rf_race = R.RaceTrack(R.track3,2)
+    rf_race = R.RaceTrack(R.track1, 2)
 
     policy = Policy(state_dims=len(rf_race.get_sizes()), n_actions=rf_race.N_ACTIONS, hidden=200)
     value = Values(rf_race)
-    trainer = Trainer(rf_race, policy, value=value, lr=1e-3, max_len=200, gamma=0.99)
+    trainer = Reinforce(rf_race, policy, value=value, lr=1e-3, max_len=200, gamma=0.99)
     for k in range(1000):
         trainer.run_episode()
